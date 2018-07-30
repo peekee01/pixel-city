50adb4de7f4d1683e6c89c5d069c0be221b95b7a
peekee01 <peekee@gmail.com>
2018-07-30 19:57:11 +0200

    einde les 1

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    @IBOutlet weak var centerMapBtnWasPressed: UIButton!
+    
+    
 }
 
+
+
+extension MapVC: MKMapViewDelegate {
+    
+}
